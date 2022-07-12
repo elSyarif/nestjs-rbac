b7556b711c31f0f5399b2b8d348246834f618686
elSyarif <id.syarif@gmail.com>
2022-07-12 16:01:35 +0700

    modify menu service repository

--- src/features/menus/menus.service.ts
+++ src/features/menus/menus.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreateMenusDto } from "./dto/create-menus.dto";
+import { UpdateMenusDto } from "./dto/update-menus.dto";
 import { Menus } from "./menus.entity";
 
 @Injectable()
@@ -11,13 +13,65 @@ export class MenusService{
         private repository: Repository<Menus>
     ){}
 
-    async create(){}
+    async create(menuDto: CreateMenusDto){
+        const menu = new Menus()
+        menu.parent_id = menuDto.parent_id
+        menu.title = menuDto.title
+        menu.icon = menuDto.icon
+        menu.link = menuDto.link
+        menu.is_active = menuDto.is_active
+        menu.sort = menuDto.sort
 
-    async findAll(){}
+        return await this.repository.save(menu)
+    }
 
-    async findOne(){}
+    async findAll(limit: number, skip: number){
+        const menus = await this.repository.find({
+            order: {
+                id: "ASC",
+                sort: "ASC"
+            }
+        })
 
-    async update(){}
+        return menus
+    }
+
+    async findOne(id: number){
+        const menu = await this.repository.findOneBy({
+            id: id
+        })
+
+        if(!menu){
+            throw new NotFoundException()
+        }
+
+        return menu
+    }
+
+    async update(menuDto: UpdateMenusDto){
+        const menu = await this.findOne(menuDto.id)
+
+        menu.title = menuDto.title
+        menu.icon = menuDto.icon
+        menu.link = menuDto.link
+        menu.is_active = menuDto.is_active
+        menu.sort = menuDto.sort
+
+        return await this.repository.save(menu)
+    }
     
-    async remove(){}
+    async remove(id: number){
+        const menu = await this.findOne(id)
+
+        return await this.repository.remove(menu)
+    }
+
+    async lastSort(id: number){
+        const menus = await this.repository.createQueryBuilder("menu")
+            .select("max(menu.sort)", "max")
+            .where("menu.parent_id = :id", {id: id})
+            .getRawOne()
+            
+        return menus
+    }
 }
